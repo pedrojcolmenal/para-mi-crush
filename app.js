235b05a00ac08cb782ca54d9b88a788481f85c3d
Pedro Colmenares <pedrojcolmenal@gmail.com>
2022-11-10 23:26:12 -0400

    ajustes de js y css

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function moverPosicionRandom(elm){
-    elm.style.position='absolute'
+    elm.style.position='absolute';
     elm.style.top=Math.random() * (window.innerHeight - elm.offsetHeight) + 'px';
     elm.style.left=Math.random() * (window.innerWidth - elm.offsetWidth) + 'px';
 }
@@ -7,13 +7,13 @@ let btnSi =document.getElementById('si');
 let btnNo =document.getElementById('no');
 let divModoSexo= document.getElementsByClassName('modo-sexo')[0];
 
-btnNo.addEventListener('mouseEnter', function(e){
+btnNo.addEventListener('mouseenter', function(e){
     moverPosicionRandom(e.target);
 });
 
 btnSi.addEventListener('click',function(e){
     alert('Sabía que dirías que sí');
     divModoSexo.style.display='block';
-    const cancion = new Audio('audio/modo_sexo.mp3');
+    const cancion = new Audio('audio/Careless_Whisper.mp3');
     cancion.play();
 })
